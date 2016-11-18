e50441879d31f100cc84352a7f0dabdc39304b12
Daniyar <dan_ntu@mail.ru>
2016-11-18 08:27:22 +0600

    Create Martov_Esep5.java
    Мөлшері m*n берілген А матрицасының k-шы бағанасының элементтерін S санына көбейтетін программа жазыңыз.

---
+++ Martov_Esep5.java
@@ -0,0 +1,45 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Esep5 {
+    public static void main(String args[]){
+        int m,n;
+        int A[][]=new int[100][100];
+         
+        Scanner sc = new Scanner(System.in);
+        System.out.println("m->");
+        m=sc.nextInt();
+        System.out.println("n->");
+        n=sc.nextInt();
+        
+        Random r = new Random();
+        for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+            A[i][j]=r.nextInt(10);
+       
+        for(int i=0;i<m;i++){
+        for (int j=0;j<n;j++){
+            System.out.print(A[i][j]+" ");    
+           }
+           System.out.println();
+        }
+        System.out.println("k->");
+        int k=sc.nextInt();
+        
+        System.out.println("S->");
+        int S=sc.nextInt();
+        
+        for (int i=0;i<m;i++)
+            A[i][k]=A[i][k]*S;
+            
+        for(int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                System.out.print(A[i][j]+" ");    
+            }
+            System.out.println();
+        }
+
+    }
+}
