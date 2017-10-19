38a8b94f1d8b8a8916b82f2eaa69e4c80fe4f41d
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-10-19 19:44:07 +0600

    Test1

--- Zadacha1.java
+++ Zadacha1.java
@@ -1,21 +1,32 @@
-public class Zadacha1 {
+class Zadacha1{
+    int a;
+    int b;
 
-    int sum(int a,int b){
+    Zadacha1(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+    public int sum(){
         return a+b;
     }
 
-    int minus(int a, int b){
-        return a-b;
-    }
-    void plural(int a,int b){
-        System.out.println("Сумма "+a*b);
+    private void sum1(){
+        System.out.println("Cумма "+(a+b));
+
     }
 
     public static void main(String[] args) {
-        Zadacha1 ob = new Zadacha1();
-        System.out.println("Сумма "+ob.sum(2,3));
-        System.out.println("Разность "+ob.minus(2,3));
-        //System.out.println("Произведение "+ob.plural(2,3));
-        ob.plural(2,3);
+
+        Zadacha1 ob = new Zadacha1(3,4);
+        Zadacha1 ob1 = new Zadacha1(3,5);
+
+        ob=ob1;
+
+        ob.sum1();
+        System.out.println("сумма = "+ob.sum());
+
+
+
     }
+
 }
