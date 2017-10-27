9382b77ca9abe90758df0b953318c1c6b581576c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-10-27 18:48:02 +0600

    lab2

---
+++ Zadacha3.java
@@ -0,0 +1,19 @@
+public class Zadacha3 {
+    int a;
+    public void  setA(int a){
+        this.a=a;
+    }
+    public int getVolume(){
+        return a*a*a;
+    }
+    public int getArea(){
+        return a*a*2;
+    }
+
+    public static void main(String[] args) {
+        Zadacha3 ob = new Zadacha3();
+        ob.setA(5);
+        System.out.println("Объем куба равна "+ob.getVolume());
+        System.out.println("Площадь боковой поверхности равна "+ob.getArea());
+    }
+}
